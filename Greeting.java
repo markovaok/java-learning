533b473fe7134441f251417faba77b23ed6fd27d
Маркова Оксана <oksmark13@gmail.com>
2025-07-13 20:28:07 +0300

    Конченная разработка

---
+++ Greeting.java
@@ -0,0 +1,11 @@
+public class Greeting {
+} package ru.mentee.power;
+
+public class Greeting {
+    public static String getGreeting(String name) {
+        return "Привет, " + name + "!";
+    }
+    public static void main(String[] args) {
+        System.out.println(getGreeting("Java разработчик"));
+    }
+}
